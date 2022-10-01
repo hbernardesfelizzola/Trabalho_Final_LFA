dcdde18065eb2be4e8d553532500080fb6518fe4
DaveTheCamper <44680026+DaveTheCamper@users.noreply.github.com>
2022-09-30 21:32:15 -0300

    Resolving read_yaml

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from yaml import *
+import yaml
 
 
+
+def read_yaml(file):
+    with open(file, "r") as stream:
+        try:
+            config = yaml.load(stream)
+            print(config)
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("\n")
+    return config
 
